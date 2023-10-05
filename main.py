b17c516baaf2e6c02834c2823ef7cba2ff8ba784
Mohamed Abdelaziz <mohamed.abdazeez@gmail.com>
2023-10-05 16:02:51 +0200

    routing - add - error handling

--- main.py
+++ main.py
@@ -3,5 +3,3 @@ from app import App
 
 routes = (('home', 123), ('home/<user>', 456), ('home/<user>/profile', 789))
 app = App(None, routes)
-
-
--- routing.py
+++ routing.py
@@ -3,13 +3,14 @@
 class Route:
 	def __init__(self):
 		# TODO [Include more than 1 dynamic child]
+		# TODO [Type hints?]
 		self.static_children = {}
 		self.dynamic_child = None
 		self.handler = None
 
 
 class Router:
-	def __init__(self, routes):
+	def __init__(self, routes: list = None):
 		self.root = Route()
 		# TODO [Decide on a more robust datastructure for segment_cache]
 		#  (capture the order to keep the most recent on top) (FILO) (Array? Stack?)
@@ -38,10 +39,13 @@ class Router:
 		node.handler = handler
 
 	def resolve(self, path: str):
-		# TODO [Convert to Async]
 		# TODO [Add Multithreading]
-		# TODO [Limit Segment Cache size (somehow)]
-		# TODO [Check static only?]
+		# TODO [Convert to Async method?]
+		# TODO [Limit Segment Cache size (somehow)] (!Important - Memory Leak)
+		# TODO [segment_cache Check static only?]
+		if not isinstance(path, str):
+			# TODO [Add Security Measures]
+			raise AttributeError('path should be a string')
 		if path in self.segment_cache:
 			return self.segment_cache[path]
 
@@ -68,5 +72,9 @@ class Router:
 		return result
 
 	def construct(self, routes):
-		for route in routes:
-			self.add_route(*route)
+		# TODO Add Default Routes for (errors)
+		if isinstance(routes, list) or isinstance(routes, set) or isinstance(routes, tuple):
+			for route in routes:
+				self.add_route(*route)
+		else:
+			raise AttributeError('routes should be a list')
