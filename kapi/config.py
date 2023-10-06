c10684a0baec21bc2bdfd47a2009c160445a2f21
Mohamed Abdelaziz <mohamed.abdazeez@gmail.com>
2023-10-06 14:45:00 +0200

    cleanup and change port

--- kapi/config.py
+++ kapi/config.py
@@ -14,7 +14,7 @@ class Config:
 	def init_config():
 		data = {}
 		data['default_attrs'] = {'separator': '/', 'param_indicators': ['<', '>']}
-		data['default_run'] = {'host': '127.0.0.1', 'port': 40001}
+		data['default_run'] = {'host': '127.0.0.1', 'port': 80}
 		return data
 
 	@staticmethod
