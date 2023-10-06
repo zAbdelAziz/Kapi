6cd9e5bb86aea3e728d58f3535b74e749e0a31b8
Mohamed Abdelaziz <mohamed.abdazeez@gmail.com>
2023-10-06 14:44:14 +0200

    create basic http requests, responses

--- kapi/requests.py
+++ kapi/requests.py
@@ -1,7 +1,33 @@
+import os
+import asyncio
+from typing import AsyncIterator
+
 
 class Request:
 	def __init__(self):
-		pass
+		self.chunk_size = 128
 
-	def process(self):
-		pass
+	@staticmethod
+	async def read_request(reader: asyncio.StreamReader):
+		lines = []
+		line = await reader.readline()
+		while line and line != b'\r\n':
+			lines.append(line)
+			line = await reader.readline()
+		return lines
+
+	async def handle(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+		x = await self.read_request(reader)
+		print(x)
+		data = bytes(f"""HTTP/1.1 200 OK\r\nContent-type: text/html\r
+				\r\n
+				<!doctype html>
+				<html>
+				<p>Tester</p>
+				</html>
+				\r\n\r\n
+				""", "utf-8")
+		writer.write(data)
+		await writer.drain()
+		writer.close()
+		print('Done')
