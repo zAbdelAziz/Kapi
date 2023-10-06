6415fb2b625ad4fc042eb5ea04151e2bf2d98ac9
Mohamed Abdelaziz <mohamed.abdazeez@gmail.com>
2023-10-06 14:44:51 +0200

    cleanup and change port

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
+
 from kapi.app import App
 
 from examples.tests_routes import routes
 
+
 if __name__ == "__main__":
-    # routes = {("/home", {"test": 123}), ("/user/<username>", {"user_handler": 456})}
-    # routes = {"/": {"main": 12}, "/home": {"test": 123}, "/user/<username>": {"user_handler": 456}}
-
-    app = App(routes=routes)
-
     host = "127.0.0.1"
     port = 80
 
-    app.run(host=host, port=port)
-
+    app = App(host=host, port=port, routes=routes)
+    app.run()
