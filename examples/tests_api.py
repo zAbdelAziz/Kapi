651499847160964be486e8bb5cf3d8fa057dc7a6
Mohamed Abdelaziz <mohamed.abdazeez@gmail.com>
2023-10-07 16:59:03 +0200

    remove prints for efficient benchmarking

--- examples/tests_api.py
+++ examples/tests_api.py
@@ -1,10 +1,10 @@
 
 async def home():
-	print('Testing Home View')
+	# print('Testing Home View')
 	return "<html><body><h1>Home View</h1></body></html>"
 
 
 async def user(uid):
-	print(f'Testing User view for {uid}')
+	# print(f'Testing User view for {uid}')
 	return {"data": uid}
 	# return f"<html><body><h1>User {uid}</h1></body></html>"
