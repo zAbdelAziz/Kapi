5a1f468b426aff5b000273b7b4e7025a01e8ac2a
Mohamed Abdelaziz <mohamed.abdazeez@gmail.com>
2023-10-07 14:22:46 +0200

    add eventloop to App

--- kapi/app.py
+++ kapi/app.py
@@ -11,10 +11,12 @@ class App:
 		self.host, self.port = host, port
 		self.config = Config(config_path)
 		self.router = Router(routes, self.config['default_attrs'])
+		self.loop = None
 
 	async def start_server(self):
 		server = await asyncio.start_server(self.handle, self.host, self.port)
 		print(f'Starting Server at http://{self.host} on {self.port}..')
+		self.loop = asyncio.get_event_loop()
 		async with server:
 			await server.serve_forever()
 
@@ -24,10 +26,15 @@ class App:
 		asyncio.run(self.start_server())
 
 	async def handle(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-		request = Request(reader=reader)
 		# TODO [Fire and forget mechanism] !!Important
+		request = Request(reader=reader)
 		await request.read()
-		method, handler, variables = self.router.resolve(request.url)
-		response = Response(method=method, handler=handler, variables=variables)
-		await response.serve(writer)
+		route = await self.router.resolve(request.url)
+		if route is not None:
+			response = Response(*route)
+			await response.serve(writer)
+		else:
+			# TODO Handle 404
+			print(f'404 Route Not Found at {request.url}!!')
+			writer.close()
 		print('Done')
