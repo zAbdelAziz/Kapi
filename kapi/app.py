066739c0649ee51180f50f40c54269e081e98c75
Mohamed Abdelaziz <mohamed.abdazeez@gmail.com>
2023-10-09 22:50:05 +0200

    separate http & ws loops in different threads

--- kapi/app.py
+++ kapi/app.py
@@ -1,6 +1,8 @@
 import asyncio
 import websockets
 
+from threading import Thread
+
 # Temporary
 # import cProfile
 # import pstats
@@ -9,8 +11,7 @@ from .config import Config
 from .routing import Router
 from .requests import Request
 
-
-# from .responses import Response
+import cProfile
 
 
 # Temporary
@@ -25,9 +26,42 @@ class App:
 		self.host, self.port, self.ws_port = host, port, ws_port
 		self.config = Config(config_path)
 		self.router = Router(routes, self.config['default_attrs'])
-		self.loop = None
-		# self.profiler = cProfile.Profile()
+		# Separate Threads, Loops and Servers
+		self.http_thread, self.ws_thread = None, None
+		self.loop, self.http_loop, self.ws_loop = None, None, None
+		self.http_server, self.ws_server = None, None
+		# Profile
+		self.profiler = cProfile.Profile()
 
+	async def start_http(self):
+		self.http_server = await asyncio.start_server(self.handle_http, self.host, self.port)
+		print(f'Starting HTTP Server at http://{self.host}:{self.port} ..')
+		await self.http_server.serve_forever()
+
+	async def start_websocket(self):
+		self.websocket_server = await websockets.serve(self.handle_websocket, self.host, self.ws_port)
+		print(f'Starting WebSocket Server at ws://{self.host}:{self.ws_port} ..')
+		await self.websocket_server.wait_closed()
+
+	async def start(self):
+		self.http_loop = asyncio.new_event_loop()
+		self.ws_loop = asyncio.new_event_loop()
+
+		self.http_thread = Thread(target=self.http_loop.run_until_complete, args=(self.start_http(),))
+		# self.http_thread.daemon = True
+
+		self.ws_thread = Thread(target=self.ws_loop.run_until_complete, args=(self.start_websocket(),))
+		# self.ws_thread.daemon = True
+
+		try:
+			self.http_thread.start()
+			self.ws_thread.start()
+		except KeyboardInterrupt:
+			self.http_server.close()
+			self.ws_server.close()
+			self.cleanup()
+
+	# Handle cleanup here
 	async def start_server(self):
 		http_server = await asyncio.start_server(self.handle_http, self.host, self.port)
 		print(f'Starting Server at http://{self.host}:{self.port} ..')
@@ -52,7 +86,9 @@ class App:
 	def run(self, host: str = None, port: int = None):
 		self.host = host if host else self.config['default_run']['host']
 		self.port = port if port else self.config['default_run']['port']
+		self.loop = asyncio.get_event_loop()
 		asyncio.run(self.start_server())
+		# asyncio.run(self.start())
 
 	async def handle_websocket(self, websocket, path):
 		# TODO [Create a separate router] (Or perhaps add some variable to the existing)
@@ -72,8 +108,8 @@ class App:
 		if request.handler is not None:
 			# self.profiler.enable()
 			await request.serve()
-		# self.profiler.disable()
-		# self.profiler.print_stats(sort='cumulative')
+			# self.profiler.disable()
+			# self.profiler.print_stats(sort='cumulative')
 		else:
 			# TODO Handle 404
 			writer.close()
